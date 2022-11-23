1b0da60228be25788be379008fadb12e980adf1b
JamesJhong <JamesJhong@trevi.cc>
2022-11-23 17:42:45 +0800

    modify card style

--- src/components/TodoCard.js
+++ src/components/TodoCard.js
@@ -6,13 +6,25 @@ const Card = styled.div`
     box-shadow: 0px 0px 2px #bdb2b2;
     background: #ffffff;
     border-radius: 5px;
+    line-height: 1;
+
+    h2 {
+        color: #151515;
+        font-size: 16px;
+        margin-bottom: 16px;
+        font-weight: bold;
+    }
+    article {
+        color: #989898;
+        font-size: 12px;
+    }
 `
 
 const RenderedCard = (props) => {
     return (
         <Card>
-            <span>{ props.title }</span>
-            <span>{ props.content }</span>
+            <h2>{ props.title }</h2>
+            <article>{ props.content }</article>
         </Card>
     )
 }
