f156893d466463688082a1a00063e314a4f0ba8d
JamesJhong <JamesJhong@trevi.cc>
2022-11-23 16:56:17 +0800

    修改 app.js 樣式

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import styled from 'styled-components';
+import Aside from './components/Aside'
+import Dashboard from './components/Dashboard'
 import './App.css';
 
-function App() {
-  return (
-    <div className="main">
-      
-    </div>
-  );
-}
+const Main = styled.main`
+  background: #FEFEFE;
+  width: 80%;
+  height: 80%;
+  border-radius: 15px;
+  box-shadow: 0px 0px 5px #bdb2b2;
+  display: flex;
+`
 
-export default App;
+const Divider = styled.span`
+  height: 100%;
+  width: 1px;
+  background: #dfdbdb;
+`
+export default () => {
+  return (
+    <Main>
+      <Aside />
+      <Divider />
+      <Dashboard />
+    </Main>
+  )
+}
