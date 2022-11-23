e2e26f05d26ea37555f9ee5f99fb1b243943a8f9
JamesJhong <JamesJhong@trevi.cc>
2022-11-23 17:29:06 +0800

    add initial todo data

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
+import { useReducer } from "react";
+import TodoCard from './TodoCard';
 
 const Dashboard = styled.section`
     padding: 20px;
@@ -15,7 +17,6 @@ const Title = styled.h1`
 `
 
 const Panels = styled.div`
-    // display: flex;
     display: grid;
     grid-template-columns: repeat(3, minmax(0, 1fr));
     grid-template-rows: 1fr;
@@ -35,21 +36,57 @@ const Panel = styled.div`
 const PanelTitle = styled.h2`
     font-size: 14px;
     color: #181818;
+    margin-bottom: 20px;
 `
 
+const initialTodos = {
+    todo: [
+        {
+            title: 'todo1',
+            content: 'todo1'
+        }
+    ],
+    inProgress: [
+        {
+            title: 'todo in progress',
+            content: 'todo in progress'
+        }
+    ],
+    completed: [
+        {
+            title: 'todo in completed',
+            content: 'todo in completed'
+        }
+    ]
+}
+
 const RenderedDashboard = () => {
+    const todoReducer = (state, action) => {
+
+    }
+    const [todos, todoDispatch] = useReducer(todoReducer, initialTodos);
+
     return (
         <Dashboard>
             <Title>Project</Title>
             <Panels>
                 <Panel>
                     <PanelTitle>To do</PanelTitle>
+                    {
+                        todos.todo.map(item => <TodoCard title={item.title} content={item.content} />)
+                    }
                 </Panel>
                 <Panel>
                     <PanelTitle>in Progress</PanelTitle>
+                    {
+                        todos.inProgress.map(item => <TodoCard title={item.title} content={item.content} />)
+                    }
                 </Panel>
                 <Panel>
                     <PanelTitle>completed</PanelTitle>
+                    {
+                        todos.completed.map(item => <TodoCard title={item.title} content={item.content} />)
+                    }
                 </Panel>
             </Panels>
         </Dashboard>
